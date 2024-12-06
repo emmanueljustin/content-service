1a8bc8874d89f0488e0343f00af40851c84a97f8
emmanueljustin <ejatienza01@gmail.com>
2024-12-06 16:38:45 +0800

    Added new serializer and create method

--- api/serializers/movie_serializer.py
+++ api/serializers/movie_serializer.py
@@ -6,5 +6,20 @@ from ..models import *
 class MovieSerializer(serializers.ModelSerializer):
   reviews = ReviewsSerializer(many=True, required=False)
   class Meta:
-    model = Movies
+    model = Movie
     fields = ['id', 'title', 'synopsis', 'genre', 'rating', 'reviews']
+
+  def create(self, validated_data):
+    reviews_data = validated_data.pop('reviews', [])
+    movie = Movie.objects.create(**validated_data)
+
+    for review_data in reviews_data:
+      Reviews.objects.create(movie=movie, **review_data)
+    
+    return movie
+  
+# Used for limiting the data shown in response when searching for movies
+class MovieSearchSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Movie
+    fields = ['id', 'title']
